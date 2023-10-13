699d038282f2b231bd2bac15fa492b846ffc3586
David Glez <david@Davids-MacBook-Pro.local>
2023-10-13 12:39:42 -0600

    Final

--- main.cpp
+++ main.cpp
@@ -5,9 +5,12 @@
 
 // Actividad 3.2 Implementación de "Dijkstra and Floyd"
 // Rogelio Guzman Cruzado - A01639914
-// David - 
+// David Alejandro González Ibarra - A01633817 
 
 using namespace std;
+
+
+const int INF = 200;
 
 // Funcion de relajacion, actualiza la distancia de un nodo a otro nodo
 void relax(vector<vector<int>>& adjmatrix, vector<int>& dist, int u, int v) {
@@ -43,18 +46,77 @@ void dijkstra(vector<vector<int>>& adjmatrix, vector<int>& dist, int src){
     }
 }
 
+
+
+vector<vector<int>> floyd(const vector<vector<int>>& graph){
+    int n = graph.size();
+    vector<vector<int>> distance(n, vector<int>(n, INF)); // Creamos el vector de vectores (matriz) que tendra las distancias 
+    // y lo inicializamos todo con numeros max para llenarla en un principio
+
+
+
+    // Aqui la inicializamos con los primeros valores del grafo y ponemos en 0 la diagonal con sus mismos numeros
+
+    for(int i=0; i<n; ++i){
+        for(int j=0; j<n; ++j){
+            if(i == j){
+                distance[i][j] = 0;
+            }
+            else if(graph[i][j]<0){
+                distance[i][j] = INF;
+            }
+            else if(graph[i][j] != 0){
+                distance[i][j] = graph[i][j];
+            }
+            
+        }
+    }
+
+    // Aqui van los 3 for loops nesteados que son la parte mas importante del algoritmo
+    // Se comparan los valores y van actualizando la matriz
+    // recorriendo todos los nodos, y en cada uno checando cada dato.
+    for(int k = 0; k<n; ++k){
+
+        for(int i = 0; i<n; ++i){
+
+            for(int j=0; j<n; ++j){
+
+                    if(distance[i][k] != INF && distance[k][j] != INF){ // Checamos que lo que vamos a revisar no sean infinitos
+                    // si no, se quedan igual
+
+                        distance[i][j] = min(distance[i][j], distance[i][k] + distance[k][j]);
+
+                        // Realizamos la comparacion para encontrar el valor minimo
+                    }
+
+            }
+        }
+    }
+
+
+    return distance;
+}
+
+
 int main() {
     int n;
     cin >> n;
     vector<vector<int>> adjmatrix(n, vector<int>(n));
+    vector<vector<int>> distanceFloyd(n, vector<int>(n));
 
+
+    // Agregamos los valores a las matrices
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
             int num;
             cin >> num;
             adjmatrix[i][j] = num;
+            distanceFloyd[i][j] = num;
         }
     }
+
+
+    vector<vector<int>> distances = floyd(distanceFloyd); // Llamamos la funcion de floyd
 
     // Cambiamos el nodo source para hacer dijkstra entre todos los nodos
     for(int src = 0; src < n; src++){
@@ -64,5 +126,19 @@ int main() {
             cout << "Node " << src + 1 << " to node " << i + 1 << " : " << dist[i] << endl;
         }
         cout << endl;
+    }
+
+    // Imprimimos el vector que nos regresa con las distancias de Floyd
+
+    for(int i=0; i < n; ++i){
+        for(int j =0; j<n; ++j){
+            if(distances[i][j] == INF){
+                cout<<"INF";
+            }
+            else{
+                cout<<distances[i][j]<<" ";
+            }
+        }
+        cout<<endl;
     }
 }
